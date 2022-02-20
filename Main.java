ee60dc3ab443090262e84b8041d5f4b3c02641a2
Jed Hazaymeh <jed.hazaymeh@gmail.com>
2022-02-20 15:17:29 +0000

    Main: option menu

--- Main.java
+++ Main.java
@@ -49,39 +49,96 @@ public class Main {
     // return new combos array
     return newCombos.toArray(new String[0]);
   }
+
+  
+  // simple function to turn an integer into an array of the integer's digits
+  public static int[] numToDigitArray(int num) {
+    String[] temp = Integer.toString(num).split("");
+    int[] numArray = new int[temp.length];
+    for(int i = 0; i < temp.length; i++) {
+      numArray[i] = Integer.parseInt(temp[i]);
+    }
+    return numArray;
+  }
+
+  public static boolean sameDigits(int[] digits) {
+    for(int i=0; i < digits.length; i++) {
+      if(digits[i] == digits[0]) {
+        continue;
+      }
+      else {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // function to convert an integer to the corresponding letters in the map object like when typing on a nokia-style keyboard where if you press 4+4+3+3+5+5+5+7 it would become the word 'help'
+  public static String generateQuery2(char[][] map) {
+    Scanner in = new Scanner(System.in);
+    in.useDelimiter(System.getProperty("line.separator")); // set scanner delimiter in order to stop scanning for new inputs when we hit a newline character/enter key
+    
+    int[][] inputNums = new int[160][];
+    int[] tempInput = new int[4];
+    String output = "";
+    for(int i=0; in.hasNextInt(); i++)
+    {
+      tempInput = numToDigitArray(in.nextInt());
+      inputNums[i] = tempInput;
+      if(inputNums[i] != null && inputNums[i].length <= map[inputNums[i][0]].length && sameDigits(inputNums[i])) {
+        output = output + map[inputNums[i][0]][inputNums[i].length-1];
+      }
+    }
+    in.close();
+    return output;
+  }
+  
+  
   
   public static void main(String[] args) {
     boolean running = true;
     boolean asking = false;
+    int option = 1;
     Scanner in = new Scanner(System.in);
 
     while (running) {
-      System.out.print("Enter a sequence of digits: ");
-      
-      try {
-        int digits = in.nextInt();
-        // Integer[] digits = handleInput(input);
-        String[] combos = generateCombos(digits);
-        System.out.println("All possible combinations are as follows:\n" + String.join(" ", combos));
+      switch (option) {
+        case 1:
+          try {
+            System.out.print("Enter a sequence of digits: ");
+            int digits = in.nextInt();
+            // Integer[] digits = handleInput(input);
+            String[] combos = generateCombos(digits);
+            System.out.println("All possible combinations are as follows:\n" + String.join(" ", combos));
+          }
+          catch (InputMismatchException ex) {
+            in.next();
+            System.out.println("Error: Input must be a sequence of digits");
+          }
+          break;
+        default:
+          System.out.println("Error: Invalid option.");
       }
-      catch (InputMismatchException ex) {
-        in.next();
-        System.out.println("Error: " + ex.toString() + " (Input must be a sequence of digits)");
-      }
-      finally {
-        asking = true;
-        while (asking) {
-          System.out.print("Do you want to enter another digit sequence? (yes|no) ");
-          String ans = in.next();
-          if (ans.equalsIgnoreCase("no")) {
-            asking = running = false;
-          }
-          else if (ans.equalsIgnoreCase("yes")) {
-            asking = false;
-          }
-          else {
-            System.out.println("Error: Invalid response. (yes|no)");
-          }
+
+      asking = true;
+      while (asking) {
+        System.out.print(
+          "\n-=== OPTION MENU ===-\n"
+          + "1) Generate all possible combinations\n"
+          + "2) Generate your own word\n"
+          + "3) Exit\n"
+          + "Select an option (integer): "
+          );
+
+        try {
+          option = in.nextInt();
+          System.out.println();
+          asking = false;
+          if (option == 3) running = false;
+        }
+        catch (InputMismatchException ex) {
+          in.next();
+          System.out.println("Error: Option must be an integer)");
         }
       }
     }
